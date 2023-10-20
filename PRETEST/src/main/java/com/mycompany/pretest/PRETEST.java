a09549311732b9c48ab935e668b7fabd4ca54e34
Nur Alifia <113223656+alipiaee@users.noreply.github.com>
2023-10-20 08:15:58 +0700

    Add files via upload

---
+++ PRETEST/src/main/java/com/mycompany/pretest/PRETEST.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pretest;
+
+/**
+ *
+ * @author SENJU
+ */
+public class PRETEST {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
